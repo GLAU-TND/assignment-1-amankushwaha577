43b5c43290f83e1d7e1933223205a002f22b1ed5
amankushwaha577 <aman.kushwaha_cs18@gla.ac.in>
2020-02-24 20:35:27 +0530

    Person class

--- src/Person.java
+++ src/Person.java
@@ -1,77 +1,68 @@
 import java.io.Serializable;
-import java.util.Scanner;
-
+/*
+ * create serialization so files can be written and read in byte form
+ * @ SuppressWarnings() - to ignore specified compiler warning in annotated element
+ */
 @SuppressWarnings("serial")
-public class Main implements Serializable {
+class Person implements Serializable {
 
-    public static void main(String[] args) {
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String anotherContactNumber;
+    private String e_mailAddress;
+    private Person next;
 
-        ContactsList newList = new ContactsList();
-        int userPressed = 0;
-        String option;
-        boolean bye = false;
-        Scanner scan = new Scanner(System.in);
-        do {
-            System.out.println("Welcome to Madhav's Contact List App\n" +
-                    "Press 1 to add a new contact\n" +
-                    "Press 2 to view all contacts\n" +
-                    "Press 3 to search for a contact\n" +
-                    "Press 4 to delete a contact\n" +
-                    "Press 5 to exit program ");
-            try {
-                userPressed = scan.nextInt();
-            } catch (Exception e) {
-                System.out.println("An Error occurred " + e);
-            }
-            switch (userPressed) {
-                case 1:
-                    System.out.println("You have chosen to add a new contact: \n" +
-                            "Please enter the name of the Person");
-                    String firstName, lastName, contactNumber, anotherNumber, EmailId;
+    /*
+     *create instance of Person and set next to 'null'
+     */
+    public Person() {
+        next = null;
+    }
 
-                    System.out.print("First Name: ");
-                    firstName = scan.next();
-                    System.out.print("Last Name: ");
-                    lastName = scan.next();
-                    System.out.print("Contact Number: ");
-                    contactNumber = scan.next();
-                    System.out.print("Would you like to add another contact number? (y/n): ");
-                    option = scan.next();
-                    if (option.equalsIgnoreCase("y")) {
-                        System.out.print("Contact Number: ");
-                        anotherNumber = scan.next();
-                    } else anotherNumber = null;
-                    System.out.print("Would you like to add email address? (y/n): ");
-                    option = scan.next();
-                    if (option.equalsIgnoreCase("y")) {
-                        System.out.print("Email Address: ");
-                        EmailId = scan.next();
-                    } else EmailId = null;
-                    System.out.println("\n");
-                    newList.addInContactsLast(firstName, lastName, contactNumber, anotherNumber, EmailId);
-                    break;
-                case 2:
-                    System.out.println("---Here are all your contacts---");
-                    newList.printContactsList();
-                    break;
-                case 3:
-                    System.out.println("You could search for a contact from their first names:");
-                    String sFirstName = scan.next();
-                    newList.searchByFirstName(sFirstName);
-                    break;
-                case 4:
-                    newList.displayContactsName();
-                    System.out.print("Press the number against the contact to delete it: ");
-                    int index = scan.nextInt();
-                    newList.deleteFromContactsList(index);
-                    break;
-                case 5:
-                    System.out.println("Exiting!");
-                    bye = true;
-                    break;
-                default:
-                    System.out.println("Unknown Entry ..");
-            }
-        } while (!bye);
+    /*
+     *getter and setter methods of firstName , lastName , contactNumber , anotherContactNumber , emailAddress and anotherE_mailAddress
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setAnotherContactNumber(String anotherContactNumber) {
+        this.anotherContactNumber = anotherContactNumber;
+    }
+
+    public void setE_mailAddress(String e_mailAddress) {
+        this.e_mailAddress = e_mailAddress;
+    }
+
+    public Person getNext() {
+        return next;
+    }
+
+    public void setNext(Person next) {
+        this.next = next;
+    }
+
+    /*
+     *  toString method @Override
+     */
+    public String toString() {
+        return "\nFirst Name: " + firstName + "\nLast Name: " + lastName + "\nContact Number: " + contactNumber + " , " + anotherContactNumber + "\nEmail Address: " + e_mailAddress;
     }
 }
